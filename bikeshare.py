04ac6b2a8030d81cc7858771d32280e866efca06
Raghad Hasan <ragad.h2002@gmail.com>
2026-05-13 12:24:25 +0300

    refactor: improve CITY_DATA formatting and clarity

--- bikeshare.py
+++ bikeshare.py
@@ -2,9 +2,13 @@ import time
 import pandas as pd
 import numpy as np
 
-CITY_DATA = { 'chicago': 'chicago.csv',
+# Dictionary mapping cities to their dataset files
+
+CITY_DATA = {
+              'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
-              'washington': 'washington.csv' }
+              'washington': 'washington.csv'
+ }
 
 def get_filters():
     """
